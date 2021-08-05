977bfd6e91d881cfa599a798934aac4d22ea9ccb
daisycoder <mkellyt@gmail.com>
2021-08-05 20:13:23 +0100

    example of dodgy site

---
+++ myprince.py
@@ -0,0 +1,42 @@
+from flask import (
+    Flask,
+    render_template
+)
+
+from flask import request
+
+# Create the application instance
+app = Flask(__name__, template_folder="templates")
+
+# Create a URL route in our application for "/"
+@app.route('/')
+def home():
+    """
+    This function just responds to the browser ULR
+    localhost:5000/
+
+    :return:        the rendered template 'home.html'
+    """
+    return render_template('index.html')
+
+
+# Create a URL route in our application for "/"
+@app.route('/account', methods=['POST','GET'])
+def getacc():
+    """
+    This function just responds to the browser ULR
+    localhost:5000/
+
+    :return:        the rendered template 'home.html'
+    """
+    print(request.method)
+    print("Hello")
+    account = request.data
+
+    # account = request.args.get('account', '')
+    print(account)
+    return "Got the account"
+
+# If we're running in stand alone mode, run the application
+if __name__ == '__main__':
+    app.run(debug=True)
